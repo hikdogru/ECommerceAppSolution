acd54bd828c31636dccbad87b3b38b11364b89f9
HALİL İBRAHİM KÜÇÜKDOĞRU <halilibrahimkucukdogru@posta.mu.edu.tr>
2023-09-21 13:14:59 +0300

    List categories to product filter area

---
+++ src/ECommerceApp.WebUI/ViewComponents/ProductFilterViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceApp.Application.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.WebUI.ViewComponents;
+
+public class ProductFilterViewComponent : ViewComponent
+{
+    private readonly ICategoryService _categoryService;
+
+    public ProductFilterViewComponent(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var parentCategories = _categoryService.GetHierarchicals();
+        return View(parentCategories);
+    }
+}
